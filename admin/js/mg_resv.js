39f9d76812006dec227d50e998692d5e5fc18305
HHHYERI <retki0703@gmail.com>
2024-03-13 16:39:26 +0900

    주석

---
+++ admin/js/mg_resv.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){           
+});
+
+$(function(){
+    $("#getSearch").click(function(){
+        if($("#resv-list").css("display") == "none"){
+            $("#resv-list").show();
+        }
+    });
+});
